e6a8498d1e4bf1a1f4fca02fd72f8827bfec54e6
Hip <hypnguyen1209@gmail.com>
2021-08-18 11:04:09 +0700

    init

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Brightness from 'expo-brightness';
 
+const setBrightness = async brightnessState => {
+    const { status } = await Brightness.requestPermissionsAsync()
+    if (status === 'granted') {
+        if (brightnessState) {
+            Brightness.useSystemBrightnessAsync()
+        } else {
+            Brightness.setBrightnessAsync(1)
+        }
+    }
+}
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [presentBrightness, setPresentBrightness] = useState(false)
+    useEffect(() => {
+        (async () => {
+            const { status } = await Brightness.requestPermissionsAsync()
+            if (status === 'granted') {
+                setPresentBrightness(true)
+                Brightness.setBrightnessAsync(1)
+            }
+        })();
+    }, [])
+    return (
+        <View style={styles.container} onStartShouldSetResponder={() => {
+            setBrightness(presentBrightness)
+            setPresentBrightness(!presentBrightness)
+        }}>
+            <Text>Vui lòng allow SYSTEM_BRIGHTNESS</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
